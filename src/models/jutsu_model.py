afe03934a457f2b5f8166aa0bf02d725f2dd4e90
Vinicius <troyackvinicius@gmail.com>
2021-10-27 19:03:16 -0300

    fix init

--- src/models/jutsu_model.py
+++ src/models/jutsu_model.py
@@ -4,20 +4,22 @@ class Jutsu():
     def __init__(self, jutsu_name, jutsu_type, jutsu_level, jutsu_damage, chakra_spend) -> None:
         self.jutsu_name = jutsu_name
         self.jutsu_type = jutsu_type
-        self.jutsu_level = jutsu_level
+        self.jutsu_level = self.jutsu_is_ranked(jutsu_level)
         self.jutsu_damage = jutsu_damage
-        self.chakra_spend = chakra_spend
-        self.jutsu_is_ranked()
-        self.spended_chakra()
+        self.chakra_spend = self.spended_chakra(chakra_spend)
 
 
-    def jutsu_is_ranked(self):
-        if not self.jutsu_level.upper() in self.jutsu_ranks:
-            self.jutsu_level = 'Unranked'
+    def jutsu_is_ranked(self, level):
+        if not level.upper() in self.jutsu_ranks:
+           return 'Unranked'
         else:
-            self.jutsu_level = self.jutsu_level.upper()
+           return level.upper()
         
-    def spended_chakra(self):
-        if self.chakra_spend <= 0:
-            self.chakra_spend = 100
+    def spended_chakra(self, spended_chakr):
+        if spended_chakr <= 0:
+           return 100
         
+
+rasengan = Jutsu('Rasengan', 'Vento', 'a', 20, -15)
+
+print(rasengan.__dict__)
